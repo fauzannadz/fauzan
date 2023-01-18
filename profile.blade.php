8b822b2e2bbcc328e73521ad1e895710dc8a2654
fauzannadz <122953841+fauzannadz@users.noreply.github.com>
2023-01-18 15:09:27 +0700

    Add files via upload

---
+++ profile.blade.php
@@ -0,0 +1,15 @@
+<html>
+     <head>
+          <body>
+          <link rel="stylesheet" href="style.css">
+           <h2 style="text-align: center"><u>profile</u></h2>
+          <p> Nama  : Reza,Ridho,Fauzan,Ryu,Taufik  </p>
+          <p> kelas : XI RPL  </p>
+          <p> Absen : 21,22,08,24,25      </p>
+          <p> Alamat: Bandung </p>
+          <img src="dali.png">
+          
+          
+      </body>
+     </head>
+</html> 
