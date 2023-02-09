8fa58891656df653f81ed0dc56e51db82336c8b5
fauzannadz <122953841+fauzannadz@users.noreply.github.com>
2023-02-09 08:51:32 +0700

    Add files via upload

---
+++ gallery.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <title>Data Guru</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div class="container">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="/siswa">Data Siswa</a>
+                </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="/guru">Data Guru</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/gallery">Gallery</a> 
+                </li>       
+            </ul>
+        </div>
+    </nav>
+    <img src="gab.jpg">
+</body>
+</html>
+    
